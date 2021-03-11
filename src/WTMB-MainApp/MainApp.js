4fbd6a0614ce9e988216380fcf4a7a40fe17ab16
Aaron <amsegal93@gmail.com>
2021-03-11 17:52:22 -0500

    Working on data structures for database and react state organization

--- src/WTMB-MainApp/MainApp.js
+++ src/WTMB-MainApp/MainApp.js
@@ -5,6 +5,22 @@ import RecipeList from '../WTMB-RecipeList/WTMB-RecipeList';
 import MarketBoardInfo from '../MarketBoardInfo/MarketBoardInfo'
 
 class MainApp extends Component {
+
+    // Test state format
+    state = {
+        savedRecipes: ['Test', 'test2', 'test3'],
+        craftingMaterials: []
+    }
+
+    deleteRecipe(recipeToDelete) {
+
+        let tempRecipes = [...this.state.savedRecipes];
+
+        tempRecipes.splice(recipeToDelete, 1);
+
+        this.setState({savedRecipes: tempRecipes})
+    }
+
     render() {
         return (
             <div className="MainAppContainer">
@@ -24,3 +40,23 @@ class MainApp extends Component {
 }
 
 export default MainApp;
+
+
+// Test dataset formatting
+
+//DATABASE
+
+/*  
+    User table -> Crafting Recipe Table -> Crafting Recipe Ingredient table (references the recipe in the parent table) -> cost table (references both the recipe and the ingredient tables {maybe})
+
+    Item Table (including full item)
+        uniqueId, userId, itemName, date, time, timeOfDay, cost, server(if needed)
+*/
+
+
+
+//STATE
+
+/*
+    
+*/
